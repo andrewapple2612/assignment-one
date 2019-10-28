472b2fc759ef2dc54fea2e36fb53f720053d2d83
Andrew Apple <rep.andrewapple@yahoo.com>
2019-10-27 22:26:39 -0500

    Creating Bio for Githug

---
+++ Bio.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var assignment = "Module 1"
+let heading = "Attempting to Code\n"
+let fullName = "Name: Andrew Apple\n"
+let age = "Age: 22 years old\n"
+let birth = "Born: Hot Springs, AR\n\n"
+let goals = "Currently, as I continue in life, I do not exactly know what my current career expectations are. Regardless of age, you can living within the unknown and yet hope to find something you will rather enjoy. At the moment, I will want to continue to work with a corporation that continues to expand in app development or SEO markeeting. \nOdd as it may seem, I am willing to go into the ministry and continue to do web or app devopment for churches of various sizes. Obstacle can completely alter a person's life, so you must be able to adapt or fight for what you know is worth it."
+var bio = heading + fullName + age + birth + goals
+print(bio)
