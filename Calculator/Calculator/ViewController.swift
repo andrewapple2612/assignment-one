685d629a73d8e099c5c895c21a7c4bb5fe84810c
andrewapple2612 <50312472+andrewapple2612@users.noreply.github.com>
2019-11-03 23:58:17 -0600

    Add files via upload

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Andrew Apple on 11/3/19.
+//  Copyright © 2019 Andrew Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func numberOne(_ sender: Any) {
+    }
+    @IBAction func rightField(_ sender: Any) {
+    }
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    finalScore = score
+    rightField =+ numberOne = score
+    finalScore = theAnswer
+}
+   
+
+
+  
