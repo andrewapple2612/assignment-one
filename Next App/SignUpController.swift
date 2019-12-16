c4c1b5bb78228306680d1052150076fe8212fcf3
andrewapple2612 <50312472+andrewapple2612@users.noreply.github.com>
2019-12-15 19:22:39 -0600

    Add files via upload

---
+++ Next App/SignUpController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpController.swift
+//  Next App
+//
+//  Created by Andrew Apple on 12/15/19.
+//  Copyright © 2019 Andrew Apple. All rights reserved.
+//
+
+import UIKit
+
+class SignUpController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
